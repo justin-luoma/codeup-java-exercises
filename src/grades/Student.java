09c593913122450b9914d08c5dddf91aefb569fd
Justin Luoma <justin@justinluoma.com>
2018-09-24 10:51:12 -0500

    Java II Collections: Exercise 1 completed

---
+++ src/grades/Student.java
@@ -0,0 +1,36 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        int sum = grades.stream()
+                .mapToInt(grade -> grade)
+                .sum();
+        return (double) sum / grades.size();
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student("justin");
+        s.addGrade(100);
+        s.addGrade(98);
+
+        System.out.println(s.getGradeAverage());
+    }
+}
